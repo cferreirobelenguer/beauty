f767b22d83457d8e5d3700019d5ec38d6dc7be8a
Carolina Ferreiro Belenguer <78199213+CarolinaFerre@users.noreply.github.com>
2022-04-10 00:32:45 +0200

    cambios en blog y cabecera tratamientos 10 abril

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,15 +2,38 @@ import React,{Component} from "react";
 import Button from '@mui/material/Button';
 import styles from '../assets/css/estilos.module.css';
 import onda from '../assets/images/onda.png';
+import bronceador from '../assets/images/bronceador.png';
+import decoracion4 from '../assets/images/decoracion4.png';
+import decoracion5 from '../assets/images/decoracion5.png';
 
 class Blog extends Component{
     render(){
+            //Función que aplica filtro gris a la foto de sobre nosotros
+    function conFiltro(e){
+        e.target.style.filter='grayscale(50%)';
+    }
+    function sinFiltro(e){
+        e.target.style.filter='grayscale(0%)';
+    }
         return(
             <div className={styles.blogFondo}>
                 {/*Onda decorativa en todas las vistas*/}
                 <div className="d-flex flex-column bd-highlight mb-3">
                         <img src={onda} width="1500" height="300" className="img-fluid"></img>
                 </div>
+                <div className="d-flex flex-column bd-highlight mb-3" id={styles.contenidoNosotros}>
+                    <div class="d-flex flex-row-reverse" data-aos="zoom-in"><img src={decoracion4} id={styles.decoracion4} width="110" heigth="110" className="img-fluid"></img></div>
+                    <div class="d-flex justify-content-center"><h1 className={styles.tituloNosotros}>Nuestro blog</h1></div>
+                    <div class="d-flex justify-content-center"><br></br></div>
+                    <div class="d-flex justify-content-center"><h6 id={styles.blogCabecera}>Si te preocupa el estado de tu piel y quieres conocer trucos, consejos y productos para mantenerla cuidada los 365 días del año, estás en el lugar adecuado. Cada estación tiene unas características y, por ello, hay que cuidar la piel de formas distintas en verano o invierno, en primavera u otoño.</h6></div>
+                    <div class="d-flex justify-content-rigth" data-aos="zoom-in"><img src={decoracion5} id={styles.decoracion5} width="110" heigth="110" className="img-fluid"></img></div>
+                    <div class="d-flex justify-content-center"><br></br></div>
+                </div>
+                <div className={styles.portadaNosotros} data-aos="fade-down">
+                    <img src={bronceador} onMouseEnter={conFiltro} onMouseLeave={sinFiltro} width="1300" height="1300" className="img-fluid"></img>
+                </div>
+                
+                
             </div>
         );
     }
--- src/components/Tratamientos.js
+++ src/components/Tratamientos.js
@@ -4,22 +4,40 @@ import higieneHydral from '../assets/images/hidralInfusion.png';
 import peeling from '../assets/images/Peeling.png';
 import higiene from '../assets/images/facialPurifying.png';
 import maquillaje from '../assets/images/maquillaje.png';
+import tratamientos from '../assets/images/portadatratamientos.jpg';
 import onda from '../assets/images/onda.png';
 import styles from '../assets/css/estilos.module.css';
+import decoracion4 from '../assets/images/decoracion4.png';
+import decoracion5 from '../assets/images/decoracion5.png';
 
 class Tratamientos extends Component{
 
     render(){
-    
+                  //Función que aplica filtro gris a la foto de sobre nosotros
+    function conFiltro(e){
+        e.target.style.filter='grayscale(50%)';
+    }
+    function sinFiltro(e){
+        e.target.style.filter='grayscale(0%)';
+    }
         return(
             <div className={styles.tratamientosFondo}>
                 {/*Onda decorativa en todas las vistas*/}
                 <div className="d-flex flex-column bd-highlight mb-3">
                         <img src={onda} width="1500" height="300" className="img-fluid"></img>
                 </div>
+                <div className="d-flex flex-column bd-highlight mb-3" id={styles.contenidoNosotros}>
+                    <div class="d-flex flex-row-reverse" data-aos="zoom-in"><img src={decoracion4} id={styles.decoracion4} width="110" heigth="110" className="img-fluid"></img></div>
+                    <div class="d-flex justify-content-center"><h1 className={styles.tituloNosotros}>Nuestros tratamientos</h1></div>
+                    <div class="d-flex justify-content-center"><br></br></div>
+                    <div class="d-flex justify-content-center"><h2 className={styles.tituloNosotros2}>Reserva ya</h2></div>
+                    <div class="d-flex justify-content-rigth" data-aos="zoom-in"><img src={decoracion5} id={styles.decoracion5} width="110" heigth="110" className="img-fluid"></img></div>
+                    <div class="d-flex justify-content-center"><br></br></div>
+                </div>
+                
                 <div className="d-flex flex-column bd-highlight mb-3" id={styles.facial}>
                     <div className="d-flex  justify-content-center">
-                        <img src={higiene} width="1200" height="800" className="img-fluid" alt="higieneFacial"/>
+                        <img src={higiene}  onMouseEnter={conFiltro} onMouseLeave={sinFiltro} width="1200" height="800" className="img-fluid" alt="higieneFacial"/>
                     </div>
                     
                     <div className="d-flex  justify-content-center" id={styles.higieneFacial}>
@@ -36,7 +54,7 @@ class Tratamientos extends Component{
                 </div>
                 <div className="d-flex flex-column bd-highlight mb-3" id={styles.hidral}>
                     <div className="d-flex  justify-content-center">
-                        <img src={higieneHydral} width="1200" height="800" className="img-fluid" alt="higieneFacial"/>
+                        <img src={higieneHydral}  onMouseEnter={conFiltro} onMouseLeave={sinFiltro} width="1200" height="800" className="img-fluid" alt="higieneFacial"/>
                     </div>
                     
                     <div className="d-flex  justify-content-center" id={styles.higieneHydral}>
@@ -52,7 +70,7 @@ class Tratamientos extends Component{
                 </div>
                 <div className="d-flex flex-column bd-highlight mb-3" id={styles.containerPeeling}>
                     <div className="d-flex  justify-content-center">
-                        <img src={peeling} width="1200" height="800" className="img-fluid" alt="higieneFacial"/>
+                        <img src={peeling}  onMouseEnter={conFiltro} onMouseLeave={sinFiltro} width="1200" height="800" className="img-fluid" alt="higieneFacial"/>
                     </div>
                     
                     <div className="d-flex  justify-content-center" id={styles.higienePeeling}>
@@ -67,7 +85,7 @@ class Tratamientos extends Component{
                 </div>
                 <div className="d-flex flex-column bd-highlight mb-3" id={styles.containerMakeUp}>
                     <div className="d-flex  justify-content-center">
-                    <img src={maquillaje} width="1200" height="800" className="img-fluid" alt="higieneFacial"/>
+                    <img src={maquillaje}  onMouseEnter={conFiltro} onMouseLeave={sinFiltro} width="1200" height="800" className="img-fluid" alt="higieneFacial"/>
                     </div>
                     
                     <div className="d-flex  justify-content-rigth" id={styles.maquillajeContenido}>
