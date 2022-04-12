ec93b1fc264c65f213b5bc0de6e9c4a454bd8e10
Carolina Ferreiro Belenguer <78199213+CarolinaFerre@users.noreply.github.com>
2022-04-12 20:46:08 +0200

    blog terminado 12 de abril 2022

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,6 +2,7 @@ import React,{Component} from "react";
 import styles from '../assets/css/estilos.module.css';
 import bronceador from '../assets/images/broncearse.png';
 import luminosidad from '../assets/images/luminosidad.png';
+import rutinas from '../assets/images/rutinas.png';
 import decoracion4 from '../assets/images/decoracion4.png';
 import decoracion5 from '../assets/images/decoracion5.png';
 import logo from '../assets/images/logo.png';
@@ -64,7 +65,7 @@ class Blog extends Component{
                         </details>
                     </div>
                     
-                    <div className={styles.portadaNosotros} data-aos="fade-down">
+                    <div className={styles.portadaNosotros} >
                         <img src={luminosidad} onMouseEnter={conFiltro} onMouseLeave={sinFiltro} width="1000" height="1000" className="img-fluid"></img>
                     </div>
                 <div className="d-flex flex-column bd-highlight mb-3" id={styles.contenidoNosotros}>
@@ -102,7 +103,52 @@ class Blog extends Component{
                     </div>
                     
                 </div>
-                
+                <div className={styles.portadaNosotros} >
+                        <img src={rutinas} onMouseEnter={conFiltro} onMouseLeave={sinFiltro} width="1000" height="1000" className="img-fluid"></img>
+                </div>
+                <div className="d-flex flex-column bd-highlight mb-3" id={styles.contenidoNosotros}>
+                    <div class="d-flex justify-content-center">
+                        
+                        <div class="d-flex justify-content-center" id={styles.bronceado}>
+                            <h2>Preparar la piel antes del maquillaje</h2>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        {/*Se abren los post cuando se pulsa el triángulo a través de las etiquetas details y summary*/}
+                        <details>
+                            <summary id={styles.contenidoBronceado}></summary>
+                            <div class="d-flex justify-content-center" id={styles.contenidoBronceado}>
+                            
+                            <h6>En la entrada de hoy quería contaros qué productos utilizo como paso previo a la base de maquillaje <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-smile" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+                                </svg><br></br>
+
+                            1. En primer lugar lo que hago es poner un poquito de agua micelar en un disco desmaquillante para dejar la piel bien limpita. Ahora estoy utilizando una especial para pieles grasas que desmaquilla, limpia y matifica. Además también es perfecta para pieles sensibles. Este punto es independiente al de la limpieza con un gel antes de acostarme y por la mañana cuando me levanto.<br></br>
+                            2. Es fundamental aplicar una hidratante por varias razones: para que el maquillaje se extienda mejor, para que aguante más tiempo intacto en el rostro , para aportar el grado de hidratación óptimo que toda piel necesita y porque en el momento que la extendemos estamos realizando un pequeño masaje que estimulará la microcirculación y en cuestión de segundos tendremos un mejor aspecto <br></br>
+                            En realidad me voy turnando varias, hoy os enseño dos: una que lleva un SPF50  y una ligerita especial para pieles normales-mixtas de rápida absorción sin ningún tipo de SPF para cuando la base de maquillaje que utilice ya tenga de por sí protección.<br></br>
+                            3. Este punto en realidad tampoco lo hago siempre y confieso que a veces me lo salto ;P El paso tres sería aplicar un primer para neutralizar o bien para afinar los poros. Cualquier producto de este tipo hará que la base dure más tiempo y contribuya a la no aparición de brillos. Si os digo la verdad los que utilizo generalmente son con partículas luminosas para obtener un acabado en la piel más glow. Sin duda mis favoritos son estos dos: el de KIKO más sutil y el «copacabana» de NARS mucho más evidente. <br></br>
+                            4. Siempre me gusta aplicar un contorno de ojos que aporte luz y se absorba rápido sin dejar una sensación pegajosa pero que a la vez hidrate. Ambas opciones son geniales <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-smile" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+                                </svg> El de Shiseido incluso reduce el color oscuro de la ojera <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-smile" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+                                </svg> Ya sabéis que debéis aplicarlo con la yema del dedo anular a toquecitos y con cuidado ya que esa piel es muy sensible y finita <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-smile" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+                                </svg><br></br>
+                            ¡Un besito!<br></br>
+
+                            ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥</h6>
+                            </div>
+                            
+                            <div class="d-flex justify-content-rigth" id={styles.bronceado}>
+                            Publicado por&nbsp;&nbsp; <Avatar alt="beautyBlog" src={logo}/>&nbsp;
+                            </div>
+                        </details>
+                        </div>
+                    </div>
                 
             </div>
         );
