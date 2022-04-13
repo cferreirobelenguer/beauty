1bfd1f80e6b71a0dfdf939292d1e3b5e99935530
Carolina Ferreiro Belenguer <78199213+CarolinaFerre@users.noreply.github.com>
2022-04-13 17:05:51 +0200

    instalacion axios y primeras pruebas en menuCita 13-04-2022

--- src/components/PideCita.js
+++ src/components/PideCita.js
@@ -1,8 +1,10 @@
 import React,{Component} from "react";
 import styles from '../assets/css/estilos.module.css';
+import axios from 'axios';
 
 class PideCita extends Component{
     render(){
+    
         return(
             <div className={styles.pideCitaFondo}>
                 
