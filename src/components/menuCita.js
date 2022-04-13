e69b9fc040e74824baf00c2cb19f896d9d7bd205
Carolina Ferreiro Belenguer <78199213+CarolinaFerre@users.noreply.github.com>
2022-04-13 17:06:42 +0200

    instalación axios y primeras pruebas en menuCitas 13-04-2022

--- src/components/menuCita.js
+++ src/components/menuCita.js
@@ -2,11 +2,23 @@ import React,{Component} from "react";
 import styles from '../assets/css/estilos.module.css';
 import decoracion4 from '../assets/images/decoracion4.png';
 import decoracion5 from '../assets/images/decoracion5.png';
+import axios from 'axios';
 
-
-
+var total=[];
 class menuCita extends Component{
+
+    
+    
     render(){
+         //Petición ajax de prueba por get
+        axios.get('http://localhost:3900/api/ver/Pepita&Perez')
+        .then(res=>{
+            //console.log(res.data.resultados2);
+            total=res.data.resultados2;
+            console.log(total);
+            console.log(total[0].apellidos);
+        
+        })
         return(
             <div className={styles.menuCitaFondo}>
                 <div className="d-flex flex-column bd-highlight mb-3" id={styles.contenidoNosotros}>
@@ -16,7 +28,9 @@ class menuCita extends Component{
                     <div class="d-flex justify-content-center"><h6 id={styles.blogCabecera}>¡Te estamos esperando!</h6></div>
                     <div class="d-flex justify-content-rigth" data-aos="zoom-in"><img src={decoracion5} id={styles.decoracion5} width="110" heigth="110" className="img-fluid"></img></div>
                     <div class="d-flex justify-content-center"><br></br></div>
+                    
                 </div>
+                
             </div>
         );
     }
